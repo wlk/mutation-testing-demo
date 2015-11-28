8c8cb4fd419c682dbb6d09b92ba34b0ab4e58207
Wojciech Langiewicz <wlangiewicz@gmail.com>
2015-11-28 19:39:16 +0100

    simple counter tests

---
+++ src/main/java/com/wlangiewicz/mutationtestingdemo/Counter.java
@@ -0,0 +1,28 @@
+package com.wlangiewicz.mutationtestingdemo;
+
+public class Counter {
+    private int count;
+    public Counter() {
+        count = 0;
+    }
+
+    public void increment() {
+        ++count;
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void make5Increments() {
+        increment();
+        increment();
+        increment();
+        increment();
+        increment();
+    }
+}
---
+++ src/test/java/com/wlangiewicz/mutationtestingdemo/CounterTest.java
@@ -0,0 +1,43 @@
+package com.wlangiewicz.mutationtestingdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ Uncommented tests have a 100% line coverage
+ But mutation coverage is only 14%
+ */
+public class CounterTest {
+
+    @Test
+    public void test_setup() {
+        Counter c = new Counter();
+        assertEquals(0, c.getCount());
+    }
+
+    @Test
+    public void test_make5increments_and_reset() {
+        Counter c = new Counter();
+        c.make5Increments(); //someone was sloppy here and just executed method to increase code coverage but didn't assert this condition
+        c.reset();
+        assertEquals(0, c.getCount());
+    }
+
+    // Uncommenting this test increases mutation coverage to 29%
+    /*@Test
+    public void test_increment_and_reset() {
+        Counter c = new Counter();
+        c.increment();
+        assertEquals(1, c.getCount());
+    }*/
+
+    // Uncommenting this test increases mutation coverage to 100%
+    /*@Test
+    public void test_make5increments() {
+        Counter c = new Counter();
+        c.make5Increments();
+        assertEquals(5, c.getCount());
+    }*/
+
+}
